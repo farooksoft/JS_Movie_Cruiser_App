5c5055ddab45899327861a0f32954c64a3e53d5f
Kumaravel <kumaravel.mamundi@cognizant.com>
2018-07-12 01:35:31 +0530

    javascript-movie-cruiser-assignment

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,109 @@
-function getMovies() {
+var allMovies = [];
+var favouriteMovies = [];
 
+function getMovies() {
+	return fetch('http://localhost:3000/movies')
+		.then(function (data) {
+			return data.json();
+		})
+		.then(function (json) {
+
+			let htmlText = '';
+			json.forEach(movie => {
+				htmlText = htmlText + 
+					`<div class="col-sm-3">
+		   			<img class="img-responsive" src="${movie.posterPath}" alt="" /><br>
+					<b><p class="card-text">${movie.title} </p></b><br>
+		   			<input type="submit" class="favbtn btn btn-info btn-secondary btn-sm" value="Add to favorites" onClick="addFavourite(${movie.id})"/>	
+					</div>
+					`;
+			});
+
+			let movieList = document.getElementById('moviesList');
+			movieList.innerHTML = htmlText;
+			allMovies = json;
+			return json;
+		})
+		.catch(function (err) {
+			return Promise.reject(error);
+		});
+}
+
+function populateFavoriteMovieList() {
+	let htmlText = '';
+	favouriteMovies.forEach(movie => {
+		htmlText = htmlText + 
+			`<div class="col-sm-3">
+			<img class="img-responsive" src="${movie.posterPath}" alt="" /><br> 
+			<b><p class="card-text">${movie.title} </p></b>
+			</div>
+			`;
+	});
+
+	let favouriteMoviesList = document.getElementById('favouritesList');
+	favouriteMoviesList.innerHTML = htmlText;
+	return favouriteMovies;
 }
 
 function getFavourites() {
+	return fetch('http://localhost:3000/favourites')
+		.then(function (data) {
+			return data.json();
+		})
+		.then(function (json) {
 
+			favouriteMovies = json;
+			return populateFavoriteMovieList();
+
+		})
+		.catch(function (err) {
+			return Promise.reject(error);
+		});
 }
 
-function addFavourite() {
+function addFavourite(movieId) {
+
+	let isPresent = false;
+	// checking if movie is present in favourite list
+	favouriteMovies.forEach(movie => {
+		if (movie.id === movieId) {
+			isPresent = true;
+		}
+	})
+	// if movie is not present in favourite list 
+	if (!isPresent) {
+		let movieToBeAdded;
+		// finding movie by movieId from all movies list
+		allMovies.forEach(movie => {
+			if (movie.id === movieId) {
+				movieToBeAdded = movie;
+			}
+		})
+		// adding movie to favouriteMovies list	
+		favouriteMovies.push(movieToBeAdded);
+
+		// making post call to save favourite movie to db
+		return fetch('http://localhost:3000/favourites', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(movieToBeAdded)
+		})
+			.then(function (res) {
+				return populateFavoriteMovieList();
+			}
+			)
+			.catch(function (error) {
+				console.log('failed to add favourites ' + error)
+				return Promise.reject(error);
+			});
+	} else {
+		alert('Movie is already added to favourites');
+		const error = new Error('Movie is already added to favourites');
+		throw error;
+	}
 
 }
 
